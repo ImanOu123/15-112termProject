de5dff58ee3e1831dc11a96f1eeb1232675781a8
Iman O <iman.o543210@gmail.coml>
2021-11-18 14:35:34 +0300

    Complete Distinguishing Sections Helper Function

--- mainTermProject.py
+++ mainTermProject.py
@@ -7,6 +7,77 @@ import cv2
 import pytesseract
 import os
 from cmu_112_graphics import *
+from pytesseract import Output
 
 
+def distinguishSections(img):
+    # perform OCR on screenshot of full page
+    custom_config = r'--oem 3 --psm 1'
+    string = pytesseract.image_to_string(img, config=custom_config)
 
+    sectionsLst = string.split("\n\n")
+    cleanSectionLst = []
+
+    # create a new list of words seperated by section
+    for i in sectionsLst:
+        cleanSectionLst.append(i.replace("\n", " ").split(" "))
+
+    # extra relevant data about the words on the image - coordinates
+    dataDict = pytesseract.image_to_data(img, output_type=Output.DICT,
+                                         config=custom_config)
+    lstWords = list(dataDict["text"])
+
+    def betterIdx(lst, word, num):
+        # extracts the nth duplicate's index
+        counter = 0
+        for i in range(len(lst)):
+            if lst[i] == word:
+                counter += 1
+            if counter == num:
+                return i
+
+    # split each section with associated indices
+    idxDict = {}
+    dupWords = {}
+
+    # links each section to the indices of the words in that section
+    for i in cleanSectionLst:
+        val = []
+        for word in i:
+            if word in lstWords:
+                val.append(betterIdx(lstWords, word, dupWords.get(word, 1)))
+            # to avoid issues with duplicates
+            dupWords[word] = dupWords.get(word, 1) + 1
+        idxDict[" ".join(i)] = val
+
+    # extracts coordinates of each word in section
+    coordDict = {}
+    for key in idxDict:
+        coordDict[key] = []
+        for val in idxDict[key]:
+            coordDict[key].append([dataDict["left"][val], dataDict["top"][val],
+                                   dataDict["width"][val],
+                                   dataDict["height"][val]])
+
+    # find min and max coord of coordinates of words to get overall coordinates
+    # for the whole section
+
+    minMaxCoord = {}
+
+    for elem in coordDict:
+        minMaxCoord[elem] = [99999, 99999, 0, 0]
+        for i in coordDict[elem]:
+            # print(i[0], i[1], minMaxCoord[elem])
+            if i[0] <= minMaxCoord[elem][0]:
+                minMaxCoord[elem][0] = i[0]
+
+            if i[0] + i[2] >= minMaxCoord[elem][2]:
+                minMaxCoord[elem][2] = i[0] + i[2]
+
+            if i[1] <= minMaxCoord[elem][1]:
+                minMaxCoord[elem][1] = i[1]
+
+            if i[1] + i[3] >= minMaxCoord[elem][3]:
+                minMaxCoord[elem][3] = i[1] + i[3]
+
+    return minMaxCoord
--- testModules.py
+++ testModules.py
@@ -78,45 +78,118 @@ from pynput.mouse import Listener
 # with Listener(on_click=on_click) as listener:
 #     listener.join()
 
+# yCoord = 0
+# xCoord = 0
+# counter = 0
+# xLis = []
+# yLis = []
+# maxX = 0
+# for text, top, left, width, height in zip(dict["text"], dict["top"],
+#                                           dict["left"], dict["width"],
+#                                           dict["height"]):
+#     if text in ["The", "cat", "(Felis", "catus)"]:
+#         if counter == 1:
+#             yCoord = top
+#             xCoord = left
+#         counter += 1
+#
+#     if counter >= 1 and yCoord - 10 < top < yCoord + 10:
+#         # img = cv2.rectangle(img, (left, top), (left + width, top + height),
+#         #                     (0, 255, 0), 2)
+#         xLis.append(left)
+#         yLis.append(top)
+#         if left == max(xLis):
+#             maxX = left + width
+#     elif counter >= 1 and xCoord - 10 < left < maxX:
+#         # img = cv2.rectangle(img, (left, top), (left + width, top + height),
+#         #                     (0, 255, 0), 2)
+#         xLis.append(left)
+#         yLis.append(top)
+# img = cv2.rectangle(img, (min(xLis), min(yLis)), (max(xLis), max(yLis)),
+#                     (255, 0, 0), 2)
+# # print(pytesseract.image_to_string(img, config=custom_config))
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+
 import cv2
 import pytesseract
 from pytesseract import Output
 
 img = cv2.imread('sampleImages/fullPage1.jpg')
+# perform OCR on screenshot of full page
 custom_config = r'--oem 3 --psm 1'
 string = pytesseract.image_to_string(img, config=custom_config)
-dict = pytesseract.image_to_data(img, output_type=Output.DICT,
-                                 config=custom_config)
 
-yCoord = 0
-xCoord = 0
-counter = 0
-xLis = []
-yLis = []
-maxX = 0
-for text, top, left, width, height in zip(dict["text"], dict["top"],
-                                          dict["left"], dict["width"],
-                                          dict["height"]):
-    if text in ["The", "cat", "(Felis", "catus)"]:
-        if counter == 1:
-            yCoord = top
-            xCoord = left
-        counter += 1
+sectionsLst = string.split("\n\n")
+cleanSectionLst = []
 
-    if counter >= 1 and yCoord - 10 < top < yCoord + 10:
-        # img = cv2.rectangle(img, (left, top), (left + width, top + height),
-        #                     (0, 255, 0), 2)
-        xLis.append(left)
-        yLis.append(top)
-        if left == max(xLis):
-            maxX = left + width
-    elif counter >= 1 and xCoord - 10 < left < maxX:
-        # img = cv2.rectangle(img, (left, top), (left + width, top + height),
-        #                     (0, 255, 0), 2)
-        xLis.append(left)
-        yLis.append(top)
-img = cv2.rectangle(img, (min(xLis), min(yLis)), (max(xLis), max(yLis)),
-                    (255, 0, 0), 2)
-print(pytesseract.image_to_string(img, config=custom_config))
+# create a new list of words seperated by section
+for i in sectionsLst:
+    cleanSectionLst.append(i.replace("\n", " ").split(" "))
+
+# extra relevant data about the words on the image - coordinates
+dataDict = pytesseract.image_to_data(img, output_type=Output.DICT,
+                                     config=custom_config)
+lstWords = list(dataDict["text"])
+
+
+def betterIdx(lst, word, num):
+    # extracts the nth duplicate's index
+    counter = 0
+    for i in range(len(lst)):
+        if lst[i] == word:
+            counter += 1
+        if counter == num:
+            return i
+
+
+# split each section with associated indices
+idxDict = {}
+dupWords = {}
+
+# links each section to the indices of the words in that section
+for i in cleanSectionLst:
+    val = []
+    for word in i:
+        if word in lstWords:
+            val.append(betterIdx(lstWords, word, dupWords.get(word, 1)))
+        # to avoid issues with duplicates
+        dupWords[word] = dupWords.get(word, 1) + 1
+    idxDict[" ".join(i)] = val
+
+# extracts coordinates of each word in section
+coordDict = {}
+for key in idxDict:
+    coordDict[key] = []
+    for val in idxDict[key]:
+        coordDict[key].append([dataDict["left"][val], dataDict["top"][val],
+                               dataDict["width"][val], dataDict["height"][val]])
+
+# find min and max coord of coordinates of words to get overall coordinates
+# for the whole section
+
+minMaxCoord = {}
+
+for elem in coordDict:
+    minMaxCoord[elem] = [99999, 99999, 0, 0]
+    for i in coordDict[elem]:
+        # print(i[0], i[1], minMaxCoord[elem])
+        if i[0] <= minMaxCoord[elem][0]:
+            minMaxCoord[elem][0] = i[0]
+
+        if i[0] + i[2] >= minMaxCoord[elem][2]:
+            minMaxCoord[elem][2] = i[0] + i[2]
+
+        if i[1] <= minMaxCoord[elem][1]:
+            minMaxCoord[elem][1] = i[1]
+
+        if i[1] + i[3] >= minMaxCoord[elem][3]:
+            minMaxCoord[elem][3] = i[1] + i[3]
+for i in minMaxCoord:
+    img = cv2.rectangle(img, (minMaxCoord[i][0], minMaxCoord[i][1]),
+                        (minMaxCoord[i][2], minMaxCoord[i][3]), (255, 0, 0), 2)
+
+
+
 cv2.imshow('img', img)
 cv2.waitKey(0)
